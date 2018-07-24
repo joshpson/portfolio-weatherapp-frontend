a7304f863e2d38b2b5336bd6ecf3a3913c8460be
joshpson <jpearson846@gmail.com>
2018-07-23 20:48:23 -0400

    even more styling

--- src/components/featuredlocation/FeaturedDetailsPane.js
+++ src/components/featuredlocation/FeaturedDetailsPane.js
@@ -2,18 +2,20 @@ import React from "react";
 import Moment from "react-moment";
 import { connect } from "react-redux";
 
-const FeaturedDetailsPane = ({ currently, daily, metric }) => {
+const FeaturedDetailsPane = ({ currently, daily, metric, windowSize }) => {
   return (
     <div className="row justify-content-between justify-content-md-center text-center  pt-2">
-      <div className="col-12 col-md-2 p-md-2 pt-0 p-2 ">
-        <i className={"detail-icon wi wi-forecast-io-" + currently.icon} />
-        <br />
-        <div className="detail-text pt-1">
-          <span className="font-weight-bold">Currently</span>
+      {windowSize < 767 ? (
+        <div className="col-12 pt-0 pb-3">
+          <i className={"detail-icon wi wi-forecast-io-" + currently.icon} />
           <br />
-          {currently.summary}
+          <div className="detail-text pt-1">
+            <span className="font-weight-bold">Currently</span>
+            <br />
+            {currently.summary}
+          </div>
         </div>
-      </div>
+      ) : null}
       <div className="col-4 col-md-2 p-2">
         <i className="wi wi-windy detail-icon" />
         <br />
@@ -88,15 +90,48 @@ const FeaturedDetailsPane = ({ currently, daily, metric }) => {
           {Math.round(currently.humidity * 100)}%
         </div>
       </div>
-      <div className="col-4 col-md-2 p-2 ">
-        <i className="wi wi-moon-alt-waning-crescent-6 detail-icon" />
+      <div className="col-4 col-md-2 p-2">
+        <i className="wi wi-humidity detail-icon" />
         <br />
         <div className="detail-text pt-1">
-          <span className="font-weight-bold">Moon Phase</span>
+          <span className="font-weight-bold">Humidity</span>
           <br />
-          {daily.moonPhase}
+          {Math.round(currently.humidity * 100)}%
         </div>
       </div>
+      {windowSize > 767 ? (
+        <div className="col-4 col-md-2 p-2">
+          <i className="wi wi-humidity detail-icon" />
+          <br />
+          <div className="detail-text pt-1">
+            <span className="font-weight-bold">Humidity</span>
+            <br />
+            {Math.round(currently.humidity * 100)}%
+          </div>
+        </div>
+      ) : null}
+      {windowSize > 767 ? (
+        <div className="col-4 col-md-2 p-2">
+          <i className="wi wi-humidity detail-icon" />
+          <br />
+          <div className="detail-text pt-1">
+            <span className="font-weight-bold">Humidity</span>
+            <br />
+            {Math.round(currently.humidity * 100)}%
+          </div>
+        </div>
+      ) : null}
+      {windowSize > 767 ? (
+        <div className="col-4 col-md-2 p-2 ">
+          <i className="wi wi-moon-alt-waning-crescent-6 detail-icon" />
+          <br />
+          <div className="detail-text pt-1">
+            <span className="font-weight-bold">Moon Phase</span>
+            <br />
+            {daily.moonPhase}
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
@@ -105,7 +140,8 @@ const mapStateToProps = state => {
   return {
     currently: state.featuredLocation.weather.currently,
     daily: state.featuredLocation.weather.daily.data[0],
-    metric: state.currentUser.metric
+    metric: state.currentUser.metric,
+    windowSize: state.windowSize
   };
 };
 
--- src/components/featuredlocation/LocationHeader.js
+++ src/components/featuredlocation/LocationHeader.js
@@ -1,16 +1,33 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const LocationHeader = ({ location, weather }) => {
+const LocationHeader = ({ location, weather, windowSize }) => {
   return (
-    <div className="row justify-content-center featured-location-header pb-3">
-      <div className="col-auto ">{location.name} </div>
-      <div className="col-auto">
-        <span>
-          {Math.round(weather.currently.temperature)}
-          <i className="wi wi-degrees" />
-        </span>
+    <div>
+      <div className="row justify-content-center featured-location-header pb-3 pb-md-0">
+        {windowSize > 767 ? (
+          <div className="col-auto ">
+            <i
+              className={
+                "header-icon wi wi-forecast-io-" + weather.currently.icon
+              }
+            />
+          </div>
+        ) : null}
+
+        <div className="col-auto ">{location.name} </div>
+        <div className="col-auto">
+          <span>
+            {Math.round(weather.currently.temperature)}
+            <i className="wi wi-degrees" />
+          </span>
+        </div>
       </div>
+      {windowSize > 767 ? (
+        <div className="row justify-content-center">
+          Currently: {weather.currently.summary}
+        </div>
+      ) : null}
     </div>
   );
 };
@@ -18,7 +35,8 @@ const LocationHeader = ({ location, weather }) => {
 const mapStateToProps = state => {
   return {
     weather: state.featuredLocation.weather,
-    location: state.featuredLocation.location
+    location: state.featuredLocation.location,
+    windowSize: state.windowSize
   };
 };
 
